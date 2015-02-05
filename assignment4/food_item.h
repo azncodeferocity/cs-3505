4b1701d8af4fe3a64bdbe394a3fd68d6203820d8
Lance Petersen <petersenlance@gmail.com>
2015-02-04 21:30:25 -0700

    Started fleshing out the food_item.h file

--- assignment4/food_item.h
+++ assignment4/food_item.h
@@ -6,3 +6,36 @@
  * 
  *
  */
+
+#ifndef FOOD_ITEM_H
+#define FOOD_ITEM_H
+
+#include<string>
+
+/*
+ * This class represents a food item. It has a UPC code to identify a 
+ * type of food item uniquely, a name, a shelf life, and a remaining 
+ * life. The shelf life defines how long a particular food item can be 
+ * stored. The remaining life tells how many more days from now the 
+ * food item can be stored.
+ */
+class food_item
+{
+  private:
+    std::string UPC_code;  // Uniquely identifies a type of food item
+    std::string name;      // The name of this food item.
+    int shelf_life;        // How long a food item of this type can be stored
+    int remaining_life;    // How much longer this food item can be stored
+
+   public:
+    food_item();           //Default constructor
+    food_item(std::string upc, int life, std::string n); // Constructor
+    
+    //Updates this food items remaining life at the start of the next day.
+    void update_life();
+    
+    //Assignment operator copies the data from rhs to this object.
+    const food_item operator= (const food_item &rhs);
+};
+
+#endif
