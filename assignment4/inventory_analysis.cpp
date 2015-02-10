8777722f4ad0e3fee10fe0910594e303397be036
Basil Vetas <basilvetas@gmail.com>
2015-02-09 17:29:04 -0700

    Final Version after testing on CADE machines

--- assignment4/inventory_analysis.cpp
+++ assignment4/inventory_analysis.cpp
@@ -1,16 +1,15 @@
 /* Author: Basil Vetas & Lance Petersen
- * Date: February 4, 2015
+ * Date: February 5, 2015
  * Course: CS 3505
  * Assignment: 4
  * 
  * This class contains the main method for the inventory analysis
- * which generates the report
+ * which generates the report.
  *
  */
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <set>
 #include <map>
 #include <iterator>
 #include "report.h"
@@ -24,7 +23,6 @@ using namespace std;
  * It will print a report containing a list of unstocked products,
  * which do not exist in any warehouse, and also a list of well
  * stocked products, which exist in two or more warehouses.
- *
  */
 int main(int argc, char* argv[])
 {
@@ -45,50 +43,31 @@ int main(int argc, char* argv[])
 
   r.generate_report(argv[1]); // generate the report by scanning through the data file argument
 
-  // Test code
-  // map<string, food_item> foods = r.get_all_foods();
-  // map<string, inventory> invts = r.get_all_inventories();
-  // for (map<string, food_item>::iterator it = foods.begin(); it != foods.end(); ++it)
-  // {
-  //   cout << "Key: " << it->first << endl;
-  //   cout << " Value: " << it->second.to_string() << endl;
-  // }
-
-  // for (map<string, inventory>::iterator it = invts.begin(); it != invts.end(); ++it)
-  // {
-  //   cout << "Key: " << it->first << endl;
-  //   cout << " Value: " << it->second.to_string() << endl;
-  // }
-  // Test code
-
   cout << "\n"; // single blank line
   cout << "Report by Basil Vetas and Lance Petersen" << endl;
   cout << "\n"; // single blank line
   cout << "Unstocked Products:" << endl;
  
-
   // determine which products do not exist in any warehouse and print below
-  // don't print any other information, and no duplicate products, for example:
-  // 0984713912 pizza
-  // 0278374752 bagels
+  // don't print any other information, and no duplicate products
+
+  // get the unstocked products
   map<string, food_item> unstocked = r.get_unstocked_products();
  
+  // print out all the unstocked products
   for(map<string, food_item>::iterator inv_it = unstocked.begin();inv_it != unstocked.end(); ++inv_it)
     cout << inv_it->second.to_string() << endl;
 
-  // test code
-  // food_item::food_item pizza("0984713912", 3, "pizza");
-  // cout << pizza.to_string() << endl;
-  // test code
-
   cout << "\n"; // single blank line
   cout << "Well-Stocked Products:" << endl;
+
   // determine which products exist in multiple warehouses (positive quantities in at least two warehouses)
-  // print out only the UPC and name, for example 
-  // 0984712812 mushroom ice cream
-  // 0278374652 seaweed cereal
+  // print out only the UPC and name
+
+  // get the well stocked products
   map<string, food_item> well_stocked = r.get_stocked_products();
  
+  // print our all the well stocked products
   for(map<string, food_item>::iterator inv_it = well_stocked.begin();inv_it != well_stocked.end(); ++inv_it)
     cout << inv_it->second.to_string() << endl;
 
