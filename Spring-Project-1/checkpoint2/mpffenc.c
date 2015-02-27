067cbc1915ee9b1d0ad62f08a3d6a85f9b425bd1
Basil Vetas <basilvetas@gmail.com>
2015-02-27 15:01:49 -0700

    added files for decoder

--- Spring-Project-1/checkpoint2/mpffenc.c
+++ Spring-Project-1/checkpoint2/mpffenc.c
@@ -59,11 +59,30 @@ static av_cold int mpff_encode_init(AVCodecContext *avctx)
 static int mpff_encode_frame(AVCodecContext *avctx, AVPacket *pkt, const AVFrame *pict, int *got_packet) 
 { 
   const AVFrame * const p = pict;
-  int ret = 0; /* return value */
 
+  int ret = 0;    /* return value */
+  int pits_per_pixel = avctx->bits_per_coded_sample; /* should be 32 */
+  int packet_size;
+  uint8_t *buf;   /* declare and 8 bit int (values from 0 to 255) */
 
+#define SIZE_BITMAPFILEHEADER 00 /* need to define size of file header */
+#define SIZE_BITMAPINFOHEADER 00 /* need to define size of info header */
 
+  /* figure out buffer size? */
+  // something like this
+  // packet_size = avctx->height * bytes_per_row * 2 + avctx->height * 4 + FF_MIN_BUFFER_SIZE;
 
+  /* try to allocate memory for packet data? */
+  if ((ret = ff_alloc_packet2(avctx, pkt, packet_size)) < 0)
+        return ret;
+  buf = pkt->data; /* make buffer point at 8 bits of packet data*/
+
+  /* create file header */
+  bytestream_put_byte(&buf, 'M');
+  bytestream_put_byte(&buf, 'P');
+  bytestream_put_byte(&buf, 'F');
+  bytestream_put_byte(&buf, 'F');
+  bytestream_put_le32(&buf, packet_size);
 
 
   /* indicates we got the packet and normal return */
